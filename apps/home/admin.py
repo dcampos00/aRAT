45c708b63179e9cb7b8c3e5958e0d1c6bbb9f0c4
Daniel Campos <dcampos00@gmail.com>
2013-02-01 15:56:56 -0300

    Modification to Admin file to align administration section with configuration files

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-from aRAT.apps.home.models import Antenna, PAD, STE, SAS_LLC, DTSR, DRX
+from aRAT.apps.home.models import Antenna
 
 admin.site.register(Antenna)
-admin.site.register(PAD)
-admin.site.register(STE)
-admin.site.register(DRX)
-admin.site.register(SAS_LLC)
-admin.site.register(DTSR)
