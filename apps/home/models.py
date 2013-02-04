da809769e6aa3da74eb279e15538053e280fe05f
Daniel Campos <dcampos00@gmail.com>
2013-02-04 16:30:19 -0300

    Added the current and requested pad fields

--- apps/home/models.py
+++ apps/home/models.py
@@ -10,11 +10,23 @@ class Antenna(models.Model):
     name = models.CharField(max_length=5)
     
     # ste configuration fields
-
-    STEs = tuple([tuple([ln, i.strip()]) for ln, i in enumerate(open(settings.CONFIGURATION_DIR+"stes.cfg")) if i.strip()])
+    STEs = tuple([tuple([ln, i.strip()]) for ln, i in enumerate(open(settings.CONFIGURATION_DIR+'stes.cfg')) if i.strip()])
 
     current_ste = models.IntegerField(null=True, choices=STEs)
     requested_ste = models.IntegerField(null=True, blank=True, choices=STEs)
+
+    # pad configuration fields
+    PADs = []
+    for pad in open(settings.CONFIGURATION_DIR+'pads.cfg').readlines():
+        pad.strip()
+        if pad:
+            pad, location = pad.split()
+            PADs.append(pad)
+
+    PADs = tuple(PADs)
+
+    current_pad = models.IntegerField(null=True, choices=PADs, unique=True)
+    requested_pad = models.IntegerField(null=True, blank=True, Choices=PADs)
 
     def __unicode__(self):
         return '%s'%self.name
