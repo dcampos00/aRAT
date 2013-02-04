14efc0c2cfaac83a3b284d9cdd2416e66723b7e0
Daniel Campos <dcampos00@gmail.com>
2013-02-04 09:14:35 -0300

    changed the directory of configuration file, this change was did with the aim of to do more portable the application

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 class Antenna(models.Model):
@@ -11,7 +11,7 @@ class Antenna(models.Model):
     
     # ste configuration fields
 
-    STEs = tuple([tuple([ln, i.strip()]) for ln, i in enumerate(open("aRAT/configuration/stes.cfg")) if i.strip()])
+    STEs = tuple([tuple([ln, i.strip()]) for ln, i in enumerate(open(settings.CONFIGURATION_DIR+"stes.cfg")) if i.strip()])
 
     current_ste = models.IntegerField(null=True, choices=STEs)
     requested_ste = models.IntegerField(null=True, blank=True, choices=STEs)
