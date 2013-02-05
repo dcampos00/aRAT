c20f65076cc307c943fef2e18b9d26691387f531
Daniel Campos <dcampos00@gmail.com>
2013-02-05 12:54:58 -0300

    Was added the PAD model

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.conf import settings
 # Create your models here.
 
@@ -15,18 +16,20 @@ class Antenna(models.Model):
     current_ste = models.IntegerField(null=True, choices=STEs)
     requested_ste = models.IntegerField(null=True, blank=True, choices=STEs)
 
-    # pad configuration fields
-    PADs = []
-    for pad in open(settings.CONFIGURATION_DIR+'pads.cfg').readlines():
-        pad.strip()
-        if pad:
-            pad, location = pad.split()
-            PADs.append(pad)
-
-    PADs = tuple(PADs)
-
-    current_pad = models.IntegerField(null=True, choices=PADs, unique=True)
-    requested_pad = models.IntegerField(null=True, blank=True, Choices=PADs)
+    requester = models.ForeignKey(User, null=True, blank=True)
+    request_date = models.DateTimeField(null=True, blank=True)
 
     def __unicode__(self):
         return '%s'%self.name
+
+class PAD(models.Model):
+    name = models.CharField(max_length=5, unique=True)
+
+    current_antenna = models.OneToOneField(Antenna, related_name="Current Antenna", null=True)
+    requested_antenna = models.ForeignKey(Antenna, related_name="Requested Antenna", null=True, blank=True)
+
+    requester = models.ForeignKey(User, null=True, blank=True)
+    request_date = models.DateTimeField(null=True, blank=True)
+
+    def __unicode__(self):
+        return 'PAD %s'%self.name
