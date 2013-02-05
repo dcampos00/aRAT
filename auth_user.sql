2f74e6e4ac5402565b76559c69d0c925282f30f6
Daniel Campos <dcampos00@gmail.com>
2013-02-05 13:26:26 -0300

    modified

--- auth_user.sql
+++ auth_user.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 05, 2013 at 01:22 PM
+-- Generation Time: Feb 05, 2013 at 01:26 PM
 -- Server version: 5.5.29
 -- PHP Version: 5.4.6-1ubuntu1.1
 
@@ -19,6 +19,90 @@ SET time_zone = "+00:00";
 --
 -- Database: `arat_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_group`
+--
+
+DROP TABLE IF EXISTS `auth_group`;
+CREATE TABLE IF NOT EXISTS `auth_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_group_permissions`
+--
+
+DROP TABLE IF EXISTS `auth_group_permissions`;
+CREATE TABLE IF NOT EXISTS `auth_group_permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `group_id` (`group_id`,`permission_id`),
+  KEY `auth_group_permissions_bda51c3c` (`group_id`),
+  KEY `auth_group_permissions_1e014c8f` (`permission_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_permission`
+--
+
+DROP TABLE IF EXISTS `auth_permission`;
+CREATE TABLE IF NOT EXISTS `auth_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `content_type_id` int(11) NOT NULL,
+  `codename` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `content_type_id` (`content_type_id`,`codename`),
+  KEY `auth_permission_e4470c6e` (`content_type_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
+
+--
+-- Dumping data for table `auth_permission`
+--
+
+INSERT INTO `auth_permission` (`id`, `name`, `content_type_id`, `codename`) VALUES
+(1, 'Can add permission', 1, 'add_permission'),
+(2, 'Can change permission', 1, 'change_permission'),
+(3, 'Can delete permission', 1, 'delete_permission'),
+(4, 'Can add group', 2, 'add_group'),
+(5, 'Can change group', 2, 'change_group'),
+(6, 'Can delete group', 2, 'delete_group'),
+(7, 'Can add user', 3, 'add_user'),
+(8, 'Can change user', 3, 'change_user'),
+(9, 'Can delete user', 3, 'delete_user'),
+(10, 'Can add content type', 4, 'add_contenttype'),
+(11, 'Can change content type', 4, 'change_contenttype'),
+(12, 'Can delete content type', 4, 'delete_contenttype'),
+(13, 'Can add session', 5, 'add_session'),
+(14, 'Can change session', 5, 'change_session'),
+(15, 'Can delete session', 5, 'delete_session'),
+(16, 'Can add site', 6, 'add_site'),
+(17, 'Can change site', 6, 'change_site'),
+(18, 'Can delete site', 6, 'delete_site'),
+(19, 'Can add log entry', 7, 'add_logentry'),
+(20, 'Can change log entry', 7, 'change_logentry'),
+(21, 'Can delete log entry', 7, 'delete_logentry'),
+(22, 'Can add antenna', 8, 'add_antenna'),
+(23, 'Can change antenna', 8, 'change_antenna'),
+(24, 'Can delete antenna', 8, 'delete_antenna'),
+(25, 'Can add settings', 9, 'add_settings'),
+(26, 'Can change settings', 9, 'change_settings'),
+(27, 'Can delete settings', 9, 'delete_settings'),
+(28, 'Can add pad', 10, 'add_pad'),
+(29, 'Can change pad', 10, 'change_pad'),
+(30, 'Can delete pad', 10, 'delete_pad');
 
 -- --------------------------------------------------------
 
@@ -48,7 +132,213 @@ CREATE TABLE IF NOT EXISTS `auth_user` (
 --
 
 INSERT INTO `auth_user` (`id`, `username`, `first_name`, `last_name`, `email`, `password`, `is_staff`, `is_active`, `is_superuser`, `last_login`, `date_joined`) VALUES
-(1, 'admin', '', '', 'dcampos00@gmail.com', 'pbkdf2_sha256$10000$GBhDx1GFXKs3$Cqxo3rB8md0j5N2Z//LO3pUUUmngaHYBnopkxoUF9+w=', 1, 1, 1, '2013-02-04 19:40:12', '2013-02-01 18:42:50');
+(1, 'admin', '', '', 'dcampos00@gmail.com', 'pbkdf2_sha256$10000$GBhDx1GFXKs3$Cqxo3rB8md0j5N2Z//LO3pUUUmngaHYBnopkxoUF9+w=', 1, 1, 1, '2013-02-04 19:40:12', '2013-02-01 18:42:50'),
+(2, 'dcampos', 'Daniel', 'Campos', 'dcampos00@gmail.com', '!', 0, 1, 0, '2013-02-04 13:02:42', '2013-02-01 18:58:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_user_groups`
+--
+
+DROP TABLE IF EXISTS `auth_user_groups`;
+CREATE TABLE IF NOT EXISTS `auth_user_groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`,`group_id`),
+  KEY `auth_user_groups_fbfc09f1` (`user_id`),
+  KEY `auth_user_groups_bda51c3c` (`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_user_user_permissions`
+--
+
+DROP TABLE IF EXISTS `auth_user_user_permissions`;
+CREATE TABLE IF NOT EXISTS `auth_user_user_permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`,`permission_id`),
+  KEY `auth_user_user_permissions_fbfc09f1` (`user_id`),
+  KEY `auth_user_user_permissions_1e014c8f` (`permission_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `common_settings`
+--
+
+DROP TABLE IF EXISTS `common_settings`;
+CREATE TABLE IF NOT EXISTS `common_settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `setting` varchar(5) NOT NULL,
+  `value` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `setting` (`setting`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `common_settings`
+--
+
+INSERT INTO `common_settings` (`id`, `setting`, `value`) VALUES
+(1, 'BLOCK', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `django_admin_log`
+--
+
+DROP TABLE IF EXISTS `django_admin_log`;
+CREATE TABLE IF NOT EXISTS `django_admin_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `action_time` datetime NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `content_type_id` int(11) DEFAULT NULL,
+  `object_id` longtext,
+  `object_repr` varchar(200) NOT NULL,
+  `action_flag` smallint(5) unsigned NOT NULL,
+  `change_message` longtext NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `django_admin_log_fbfc09f1` (`user_id`),
+  KEY `django_admin_log_e4470c6e` (`content_type_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Dumping data for table `django_admin_log`
+--
+
+INSERT INTO `django_admin_log` (`id`, `action_time`, `user_id`, `content_type_id`, `object_id`, `object_repr`, `action_flag`, `change_message`) VALUES
+(1, '2013-02-01 19:14:21', 1, 8, '1', 'DV01', 2, 'Changed current_ste.'),
+(2, '2013-02-01 19:15:12', 1, 8, '1', 'DV01', 2, 'Changed current_ste.'),
+(3, '2013-02-04 19:40:39', 1, 8, '1', 'DV01', 2, 'Changed current_pad.'),
+(4, '2013-02-04 20:26:10', 1, 8, '65', 'CM12', 2, 'Changed current_pad.'),
+(5, '2013-02-04 20:41:06', 1, 8, '49', 'DA65', 2, 'Changed current_pad.'),
+(6, '2013-02-04 20:41:30', 1, 8, '9', 'DV09', 2, 'Changed current_pad.'),
+(7, '2013-02-04 21:13:46', 1, 10, '202', 'PAD object', 2, 'Changed current_antenna.'),
+(8, '2013-02-04 21:23:57', 1, 10, '199', 'PAD TF07', 2, 'Changed current_antenna.'),
+(9, '2013-02-05 13:22:43', 1, 10, '4', 'PAD T701', 2, 'Changed current_antenna.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `django_content_type`
+--
+
+DROP TABLE IF EXISTS `django_content_type`;
+CREATE TABLE IF NOT EXISTS `django_content_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `app_label` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `app_label` (`app_label`,`model`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+--
+-- Dumping data for table `django_content_type`
+--
+
+INSERT INTO `django_content_type` (`id`, `name`, `app_label`, `model`) VALUES
+(1, 'permission', 'auth', 'permission'),
+(2, 'group', 'auth', 'group'),
+(3, 'user', 'auth', 'user'),
+(4, 'content type', 'contenttypes', 'contenttype'),
+(5, 'session', 'sessions', 'session'),
+(6, 'site', 'sites', 'site'),
+(7, 'log entry', 'admin', 'logentry'),
+(8, 'antenna', 'home', 'antenna'),
+(9, 'settings', 'common', 'settings'),
+(10, 'pad', 'home', 'pad');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `django_session`
+--
+
+DROP TABLE IF EXISTS `django_session`;
+CREATE TABLE IF NOT EXISTS `django_session` (
+  `session_key` varchar(40) NOT NULL,
+  `session_data` longtext NOT NULL,
+  `expire_date` datetime NOT NULL,
+  PRIMARY KEY (`session_key`),
+  KEY `django_session_c25c2c28` (`expire_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `django_session`
+--
+
+INSERT INTO `django_session` (`session_key`, `session_data`, `expire_date`) VALUES
+('45fd474486feffd9b67048c77e5a435c', 'NDU2MTlmZjcyNTE3YWJkODgxYzk4OTc1MWI3Y2U5NDJkOWQyOTRhZjqAAn1xAShVEl9hdXRoX3Vz\nZXJfYmFja2VuZHECVSlkamFuZ28uY29udHJpYi5hdXRoLmJhY2tlbmRzLk1vZGVsQmFja2VuZHED\nVQ1fYXV0aF91c2VyX2lkcQSKAQF1Lg==\n', '2013-02-18 19:40:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `django_site`
+--
+
+DROP TABLE IF EXISTS `django_site`;
+CREATE TABLE IF NOT EXISTS `django_site` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `domain` varchar(100) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `django_site`
+--
+
+INSERT INTO `django_site` (`id`, `domain`, `name`) VALUES
+(1, 'example.com', 'example.com');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `auth_group_permissions`
+--
+ALTER TABLE `auth_group_permissions`
+  ADD CONSTRAINT `group_id_refs_id_3cea63fe` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
+  ADD CONSTRAINT `permission_id_refs_id_a7792de1` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`);
+
+--
+-- Constraints for table `auth_permission`
+--
+ALTER TABLE `auth_permission`
+  ADD CONSTRAINT `content_type_id_refs_id_728de91f` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`);
+
+--
+-- Constraints for table `auth_user_groups`
+--
+ALTER TABLE `auth_user_groups`
+  ADD CONSTRAINT `group_id_refs_id_f0ee9890` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
+  ADD CONSTRAINT `user_id_refs_id_831107f1` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+
+--
+-- Constraints for table `auth_user_user_permissions`
+--
+ALTER TABLE `auth_user_user_permissions`
+  ADD CONSTRAINT `permission_id_refs_id_67e79cb` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  ADD CONSTRAINT `user_id_refs_id_f2045483` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+
+--
+-- Constraints for table `django_admin_log`
+--
+ALTER TABLE `django_admin_log`
+  ADD CONSTRAINT `content_type_id_refs_id_288599e6` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
+  ADD CONSTRAINT `user_id_refs_id_c8665aa` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
