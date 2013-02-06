46dc76319929ac87e79ea109e98837bdce75d77f
Daniel Campos <dcampos00@gmail.com>
2013-02-06 12:12:57 -0300

    Added restriction to unassign all antennas to the PAD

--- apps/home/ajax.py
+++ apps/home/ajax.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime
 from dajax.core import Dajax
 from dajaxice.decorators import dajaxice_register
 
@@ -7,6 +8,8 @@ from django.db.models import F, Q
 
 from aRAT.apps.common.models import settings as app_settings
 
+DATE_FORMAT = "%Y-%m-%d" 
+TIME_FORMAT = "%H:%M:%S"
 
 @dajaxice_register
 def ste_update_alerts(request, ste_id='', antenna_id=''):
@@ -31,13 +34,31 @@ def ste_update_alerts(request, ste_id='', antenna_id=''):
             antenna.requested_ste = ste_id
         else:
             antenna.requested_ste = None
+        antenna.requester = request.user
+        antenna.request_date = datetime.now()
         antenna.save()
 
     # is loaded all current status of the ste configuration
     antennas = Antenna.objects.all()
     for antenna in antennas:
+        requester_first_name = None
+        requester_last_name = None
+        request_date = None
+        request_time = None
+
+        if antenna.requester != None:
+            requester_first_name = antenna.requester.first_name
+            requester_last_name = antenna.requester.last_name
+
+        if antenna.request_date != None:
+            request_date = antenna.request_date.strftime(DATE_FORMAT)
+            request_time = antenna.request_date.strftime(TIME_FORMAT)
+
         dajax.add_data({'antenna': {'id': antenna.id, 'name': antenna.name},
-                        'ste': {'id': antenna.requested_ste, 'name': antenna.get_requested_ste_display()}},
+                        'ste': {'id': antenna.requested_ste, 'name': antenna.get_requested_ste_display()},
+                        'user': {'first_name': requester_first_name, 'last_name': requester_last_name},
+                        'datetime': {'date': request_date, 'time': request_time}
+                        },
                        'update_status')
 
     return dajax.json()
@@ -61,10 +82,16 @@ def pad_update_alerts(request, pad_id='', antenna_id=''):
     # first the pad information is updated
     if pad_id != '':
         pad = PAD.objects.get(id=pad_id)
-        if antenna_id != '' and pad.current_antenna != Antenna.objects.get(id=antenna_id):
+        if antenna_id == 'None' and pad.current_antenna != None:
+            pad.assigned = False
+        elif antenna_id != '' and pad.current_antenna != Antenna.objects.get(id=antenna_id):
             pad.requested_antenna = Antenna.objects.get(id=antenna_id)
+            pad.assigned = True
         else:
             pad.requested_antenna = None
+            pad.assigned = True
+#        pad.requester = request.user
+#        pad.request_date = datetime.now()
         pad.save()
 
     # is loaded all current status of the ste configuration
@@ -77,7 +104,7 @@ def pad_update_alerts(request, pad_id='', antenna_id=''):
                 if pad != pad2:
                     if pad.requested_antenna == pad2.requested_antenna:
                         pad_errors.append(pad2.name)
-                    elif pad.requested_antenna == pad2.current_antenna:
+                    elif pad.requested_antenna == pad2.current_antenna and pad2.requested_antenna == None:
                         pad_errors.append(pad2.name)
                     
 
@@ -86,13 +113,13 @@ def pad_update_alerts(request, pad_id='', antenna_id=''):
 
         if pad.current_antenna:
             current_antenna_name = pad.current_antenna.name
-            current_antenna_id = pad.current_antenna_id
+            current_antenna_id = pad.current_antenna.id
         if pad.requested_antenna:
             requested_antenna_name = pad.requested_antenna.name
             requested_antenna_id = pad.requested_antenna.id
 
-        dajax.add_data({'pad': {'id': pad.id, 'name': pad.name},
-                        'current_antenna': {'id': current_antenna_name, 'name': current_antenna_name},
+        dajax.add_data({'pad': {'id': pad.id, 'name': pad.name, 'assigned': pad.assigned},
+                        'current_antenna': {'id': current_antenna_id, 'name': current_antenna_name},
                         'requested_antenna': {'id': requested_antenna_id, 'name': requested_antenna_name},
                         'error': [e for e in pad_errors]
                         },
