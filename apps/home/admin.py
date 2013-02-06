f978a24c8e5ec57719824d04d8233064f6ab679d
Daniel Campos <dcampos00@gmail.com>
2013-02-06 17:35:20 -0300

    Registered Correlator and CentralLO to administration sub-system

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from aRAT.apps.home.models import Antenna, PAD
+from aRAT.apps.home.models import Antenna, PAD, Correlator, CentralLO
 
 admin.site.register(Antenna)
 admin.site.register(PAD)
+admin.site.register(Correlator)
+admin.site.register(CentralLO)
