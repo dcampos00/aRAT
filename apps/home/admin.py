bf137d47e5607c3d7d8c9bb00a958e9422a53448
Daniel Campos <dcampos00@gmail.com>
2013-02-07 18:21:00 -0300

    The administration app was changed to display and manage in a better way the Correlator configurations and The Antennas

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from aRAT.apps.home.models import Antenna, PAD, Correlator, CentralLO
+from aRAT.apps.home.models import Antenna, PAD, CorrelatorConfiguration, CentralLO
 
-admin.site.register(Antenna)
+class AntennaAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'current_ste')
+    list_filter = ['current_ste', 'active']
+    search_fields = ['name']
+
+class CorrelatorConfigurationAdmin(admin.ModelAdmin):
+    list_display = ('caimap', 'line_number', 'configuration', 'correlator')
+    list_display_links = ('caimap', 'line_number', 'configuration')
+    list_filter = ['correlator', 'active']
+    search_fields = ['line', 'correlator', 'current_antenna__name', 'requested_antenna__name']
+    ordering = ['line']
+
+admin.site.register(Antenna, AntennaAdmin)
 admin.site.register(PAD)
-admin.site.register(Correlator)
+admin.site.register(CorrelatorConfiguration, CorrelatorConfigurationAdmin)
 admin.site.register(CentralLO)
