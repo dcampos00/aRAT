254cbcdce6cf4808e8f91806b935944d06444766
Daniel Campos <dcampos00@gmail.com>
2013-02-08 11:48:55 -0300

    Were model the choices outside the class

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
+COMMON_SETTINGS = (
+    (u'BLOCK', u'Block'),
+    )
+
 # Create your models here.
 class settings(models.Model):
     """
     Model that define the settings of the application, e.g: status of the system
     block and unblock
-    """    
-    
-    COMMON_SETTINGS = (
-        ('BLOCK', 'Block'),
-        )
+    """
 
     setting = models.CharField(max_length=5, choices=COMMON_SETTINGS, unique=True)
     value = models.BooleanField(default=False)
