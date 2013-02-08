73434ae71c5d977af0ec0937fb83797b827edf69
Daniel Campos <dcampos00@gmail.com>
2013-02-08 11:52:46 -0300

    Changes to the models - Was added a model for Holography Configurations - Were included the restrictions in CentralloConfiguration Model

--- apps/home/models.py
+++ apps/home/models.py
@@ -33,7 +33,7 @@ class PAD(models.Model):
     _LINES = tuple(_LINES)
 
     line = models.IntegerField(choices=_LINES, unique=True)
-    location = models.TextField(blank=True)
+    location = models.CharField(max_length=10, blank=True)
     assigned = models.BooleanField(default=True, blank=True)
     active = models.BooleanField(default=True, blank=True)
 
@@ -93,6 +93,9 @@ class PAD(models.Model):
     def name(self):
         return self.get_line_display().split()[0]
 
+    def line_number(self):
+        return self.line
+
     def save(self):
         self.location = self.get_line_display().split()[1]
         super(PAD, self).save()
@@ -100,34 +103,9 @@ class PAD(models.Model):
     def __unicode__(self):
         return 'PAD %s'%self.name()
 
-class CentralLO(models.Model):
-    # in the db is saved the line of the configuration file that match with the configuration of
-    # the CentralLO
-    LINES = []
-    for line_number, line in enumerate(open(settings.CONFIGURATION_DIR+'clo.cfg')):
-        line.strip()
-        if line:
-            line = line.strip().split()[0:-1]
-            LINES.append((line_number, str(line)[2:-2].replace("', '"," ")))
-
-    LINES = tuple(LINES)
-
-    line = models.IntegerField(choices=LINES, unique=True)
-    assigned = models.BooleanField(default=True, blank=True)
-    active = models.BooleanField(default=True, blank=True)
-
-    current_antenna = models.OneToOneField(Antenna, related_name="current_clo_antenna", null=True)
-    requested_antenna = models.ForeignKey(Antenna, related_name="requested_clo_antenna", null=True, blank=True)
-
-    requester = models.ForeignKey(User, null=True, blank=True)
-    request_date = models.DateTimeField(null=True, blank=True)
-
-    def __unicode__(self):
-        return "%s"%(self.get_line_display())
-
 class CorrelatorConfiguration(models.Model):
     # in the db is saved the line of the configuration file that match with the configuration of
-    # the CentralLO
+    # the CorrelatorConfiguration
     _LINES = []
     for line_number, line_string in enumerate(open(settings.CONFIGURATION_DIR+'corr.cfg')):
         if line_string[0] != "#":
@@ -140,7 +118,7 @@ class CorrelatorConfiguration(models.Model):
     line = models.IntegerField(choices=_LINES, unique=True)
 
     # this is a calculated field from line display value
-    correlator = models.TextField(blank=True)
+    correlator = models.CharField(max_length=10, blank=True)
     assigned = models.BooleanField(default=True, blank=True)
     active = models.BooleanField(default=True, blank=True)
 
@@ -227,3 +205,176 @@ class CorrelatorConfiguration(models.Model):
 
     def __unicode__(self):
         return "Line %s - %s [%s]"%(self.line, self.configuration(), self.correlator)
+
+class CentralloConfiguration(models.Model):
+    # in the db is saved the line of the configuration file that match with the configuration of
+    # the CentralLO
+    _LINES = []
+    for line_number, line_string in enumerate(open(settings.CONFIGURATION_DIR+'clo.cfg')):
+        if line_string[0] != "#":
+            line_string = line_string.strip()
+            if line_string:
+                _LINES.append((line_number, line_string))
+
+    _LINES = tuple(_LINES)
+
+    line = models.IntegerField(choices=_LINES, unique=True)
+
+    #calculated field
+    centrallo = models.CharField(max_length=10, blank=True)
+    assigned = models.BooleanField(default=True, blank=True)
+    active = models.BooleanField(default=True, blank=True)
+
+    current_antenna = models.OneToOneField(Antenna, related_name="current_clo_antenna", null=True)
+    requested_antenna = models.ForeignKey(Antenna, related_name="requested_clo_antenna", null=True, blank=True)
+
+    requester = models.ForeignKey(User, null=True, blank=True)
+    request_date = models.DateTimeField(null=True, blank=True)
+
+    def restriction_errors(self):
+        errors = {}
+        errors['local'] = self.local_restriction_errors()
+        errors['global'] = self.global_restriction_errors()
+        return errors
+
+    def local_restriction_errors(self):
+        configuration_errors = []
+        
+        for clo_config in CentralloConfiguration.objects.all():
+            if self != clo_config and clo_config.active:
+                if self.requested_antenna == None:
+                    if (self.current_antenna == clo_config.requested_antenna
+                        and self.current_antenna != None):
+                        configuration_errors.append(clo_config.configuration())
+                    elif (self.current_antenna == clo_config.current_antenna
+                          and clo_config.requested_antenna == None
+                          and clo_config.assigned == True):
+                        configuration_errors.append(clo_config.configuration())
+                else:
+                    if self.requested_antenna == clo_config.requested_antenna:
+                        configuration_errors.append(clo_config.configuration())
+                    elif (self.requested_antenna == clo_config.current_antenna
+                          and clo_config.requested_antenna == None
+                          and clo_config.assigned == True):
+                        configuration_errors.append(clo_config.configuration())
+
+        if configuration_errors == []:
+            configuration_errors = None
+
+        return configuration_errors
+
+    def global_restriction_errors(self):
+        if self.requested_antenna != None:
+            antenna = self.requested_antenna
+        elif self.current_antenna != None:
+            antenna = self.current_antenna
+        else:
+            return False
+
+        if antenna.requested_ste != None:
+            ste = antenna.get_requested_ste_display()
+        elif antenna.current_ste != None:
+            ste = antenna.get_current_ste_display()
+        else:
+            return True
+
+        if self.centrallo == 'AOS' and (ste == 'AOS' or ste == 'AOS2'):
+            return False
+        elif self.centrallo == 'TFINT' and (ste == 'TFINT'):
+            return False
+        else:
+            return True
+
+    def line_number(self):
+        return "%d"%self.line
+
+    def configuration(self):
+        configuration = str(self.get_line_display().split()[0:-1]).replace("', u'", ' ')[3:-2]
+        return "%s"%configuration
+
+    def save(self):
+        self.centrallo = self.get_line_display().split()[-1]
+        super(CentralloConfiguration, self).save()
+
+    def __unicode__(self):
+        return "Line %s - %s [%s]"%(self.line, self.configuration(), self.centrallo)
+
+class HolographyConfiguration(models.Model):
+    _LINES = []
+    for line_number, line_string in enumerate(open(settings.CONFIGURATION_DIR+'holography.cfg')):
+        if line_string[0] != "#":
+            line_string = line_string.strip()
+            if line_string:
+                _LINES.append((line_number, line_string))
+
+    _LINES = tuple(_LINES)
+
+    line = models.IntegerField(choices=_LINES, unique=True)
+
+    #calculated field
+    assigned = models.BooleanField(default=True, blank=True)
+    active = models.BooleanField(default=True, blank=True)
+
+    current_antenna = models.OneToOneField(Antenna, related_name="current_holo_antenna", null=True)
+    requested_antenna = models.ForeignKey(Antenna, related_name="requested_holo_antenna", null=True, blank=True)
+
+    requester = models.ForeignKey(User, null=True, blank=True)
+    request_date = models.DateTimeField(null=True, blank=True)
+
+    def restriction_errors(self):
+        errors = {}
+        errors['local'] = self.local_restriction_errors()
+        errors['global'] = self.global_restriction_errors()
+        return errors
+
+    def local_restriction_errors(self):
+        configuration_errors = []
+        
+        for holo_config in HolographyConfiguration.objects.all():
+            if self != holo_config and holo_config.active:
+                if self.requested_antenna == None:
+                    if (self.current_antenna == holo_config.requested_antenna
+                        and self.current_antenna != None):
+                        configuration_errors.append(holo_config.configuration())
+                    elif (self.current_antenna == holo_config.current_antenna
+                          and holo_config.requested_antenna == None
+                          and holo_config.assigned == True):
+                        configuration_errors.append(holo_config.configuration())
+                else:
+                    if self.requested_antenna == holo_config.requested_antenna:
+                        configuration_errors.append(holo_config.configuration())
+                    elif (self.requested_antenna == holo_config.current_antenna
+                          and holo_config.requested_antenna == None
+                          and holo_config.assigned == True):
+                        configuration_errors.append(holo_config.configuration())
+
+        if configuration_errors == []:
+            configuration_errors = None
+
+        return configuration_errors
+
+    def global_restriction_errors(self):
+        if self.requested_antenna != None:
+            antenna = self.requested_antenna
+        elif self.current_antenna != None:
+            antenna = self.current_antenna
+        else:
+            return False
+
+        if antenna.requested_ste != None:
+            ste = antenna.get_requested_ste_display()
+        elif antenna.current_ste != None:
+            ste = antenna.get_current_ste_display()
+        else:
+            return True
+
+        if ste == 'TFOHG':
+            return False
+        else:
+            return True
+
+    def name(self):
+        return "%s"%self.get_line_display()
+
+    def line_number(self):
+        return "%d"%self.line
