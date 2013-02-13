31b4eba78c116ed741bd75d0e2591419e248da44
Daniel Campos <dcampos00@gmail.com>
2013-02-13 16:29:35 -0300

    Implemented the web service This web service returns the last request information from the DB

--- apps/webServices/changesPage/views.py
+++ apps/webServices/changesPage/views.py
@@ -1,7 +1,7 @@
 from django.views.decorators.csrf import csrf_exempt
 
 from spyne.server.django import DjangoApplication
-from spyne.model.primitive import Integer, String, Boolean
+from spyne.model.primitive import String
 from spyne.service import ServiceBase
 from spyne.interface.wsdl import Wsdl11
 from spyne.protocol.soap import Soap11
@@ -9,21 +9,50 @@ from spyne.protocol.http import HttpRpc
 from spyne.application import Application
 from spyne.decorator import rpc
 
-class changesPageService(ServiceBase):
-    @rpc(String, _returns=String)
-    def changes(ctx, debug):
-        if debug:
-            return 'FAILURE'
-        else:
-            result = '<changes>'
-            for i in xrange(10):
-                result += '<change>CHANGE # %s</change>'%(i)
-            result += '</changes>'
-            return result
+from aRAT.apps.home.models import History
 
-changes_page_service = csrf_exempt(DjangoApplication(Application([changesPageService],
-                                                                   'cl.alma.arat.webservice.changesPage',
-                                                                   in_protocol=Soap11(),
-                                                                   out_protocol=Soap11(),
-                                                                   interface=Wsdl11(),
-                                                                   )))
+import re
+
+class changesPageService(ServiceBase):
+    """Class that expose the method changes, this method
+    returns a XML with the changes made in the lastest request
+    """
+    @rpc(_returns=String)
+    def changes(ctx):
+        """Return a string with the XML of the last changes"""
+        # is loaded the last request from the DB
+        request = History.objects.reverse()[:1][0].request
+        request = request.split('\n')
+
+        result = "<changes>"
+        
+        # is created the XML
+        _open = False
+        for r in request:
+            if not _open and r: # if r is not empty
+                result += "\n<change>\n"
+                _open = True
+            result += "%s\n"%r
+            if re.match("-- Request", r) is not None:
+                result += "</change>"
+                _open = False
+
+        result += "</changes>"
+
+        return result
+#        if debug:
+#            return 'FAILURE'
+#        else:
+#            result = '<changes>'
+#            for i in xrange(10):
+#                result += '<change>CHANGE # %s</change>'%(i)
+#            result += '</changes>'
+#            return result
+
+changes_page_service = csrf_exempt(
+    DjangoApplication(Application([changesPageService],
+                                  'cl.alma.arat.webservice.changesPage',
+                                  in_protocol=Soap11(),
+                                  out_protocol=Soap11(),
+                                  interface=Wsdl11(),
+                                  )))
