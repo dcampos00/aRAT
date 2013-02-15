e0db37c4401715b18bf84ac21200e96c5c34de7a
Daniel Campos <dcampos00@gmail.com>
2013-02-15 13:53:49 -0300

    The file was changed to use the new AdminSite

--- urls.py
+++ urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,19 +8,15 @@ admin.autodiscover()
 from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 dajaxice_autodiscover()
 
+from aRAT.apps.home.admin import custom_admin_site
+
 urlpatterns = patterns('',
     # Web Services URLs
     url(r'^', include('aRAT.apps.webServices.urls')),
     url(r'^', include('aRAT.apps.home.urls')),
-    # Examples:
-    # url(r'^$', 'aRAT.views.home', name='home'),
-    # url(r'^aRAT/', include('aRAT.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    # Admin
+    url(r'^admin/', include(custom_admin_site.urls)),
 
     # Dajaxice
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
