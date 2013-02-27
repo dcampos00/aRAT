349c3c6338216c46298dc11631638817fe7972b4
Daniel Campos <dcampos00@gmail.com>
2013-02-27 11:35:50 -0300

    Remove unnecessary code
    Was remove some lines that were unused

--- apps/webServices/applyChanges/views.py
+++ apps/webServices/applyChanges/views.py
@@ -1,11 +1,10 @@
 from django.views.decorators.csrf import csrf_exempt
 
 from spyne.server.django import DjangoApplication
-from spyne.model.primitive import Integer, String, Boolean
+from spyne.model.primitive import String
 from spyne.service import ServiceBase
 from spyne.interface.wsdl import Wsdl11
 from spyne.protocol.soap import Soap11
-from spyne.protocol.http import HttpRpc
 from spyne.application import Application
 from spyne.decorator import rpc
 
